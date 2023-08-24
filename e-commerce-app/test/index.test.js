8583d78f6cae0a1789438dcde5bc5d3e499de469
iamabhishekt <abhishekthakur.art@protonmail.com>
2023-08-23 21:35:43 -0400

    test 1 passed

--- e-commerce-app/test/index.test.js
+++ e-commerce-app/test/index.test.js
@@ -1,11 +1,24 @@
-import {expect, describe, test} from '@jest/globals'
+import {expect, describe, test, jest} from '@jest/globals'
 import Payment from '../src/events/payments.js';
 import Marketing from '../src/observers/marketing.js';
 import Shipment from '../src/observers/shipment.js';
 import PaymentSubject from '../src/subjects/paymentSubject.js';
 
 describe('Test Suite for Observer Pattern', () => {
-  test.todo('#PaymentSubject notify observers');
+  test('#PaymentSubject notify observers', () => {
+    const subject = new PaymentSubject()
+    const observer = {
+      update: jest.fn()
+    }
+    const data = 'hello world';
+    const expected = data;
+
+    subject.subscribe(observer)
+    subject.notify(data)
+
+    expect(observer.update).toBeCalledWith(expected)
+  });
+
   test.todo('#PaymentSubject should not notify unsubscribed observers');
   test.todo('#Payment should  notify subject after a credit card transaction');
   test.todo('#All should notify subscribers after a credit card payment');
