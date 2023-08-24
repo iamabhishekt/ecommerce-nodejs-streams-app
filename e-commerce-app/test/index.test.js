ec7a0f2589b9e2d0bb66e533ae570f7bfcfbe878
iamabhishekt <abhishekthakur.art@protonmail.com>
2023-08-23 21:51:13 -0400

    passed all test cases

--- e-commerce-app/test/index.test.js
+++ e-commerce-app/test/index.test.js
@@ -13,13 +13,58 @@ describe('Test Suite for Observer Pattern', () => {
     const data = 'hello world';
     const expected = data;
 
-    subject.subscribe(observer)
-    subject.notify(data)
+    subject.subscribe(observer);
+    subject.notify(data);
 
-    expect(observer.update).toBeCalledWith(expected)
+    expect(observer.update).toBeCalledWith(expected);
   });
 
-  test.todo('#PaymentSubject should not notify unsubscribed observers');
-  test.todo('#Payment should  notify subject after a credit card transaction');
-  test.todo('#All should notify subscribers after a credit card payment');
+  test('#PaymentSubject should not notify unsubscribed observers', () => {
+    const subject = new PaymentSubject()
+    const observer = {
+      update: jest.fn()
+    }
+    const data = 'hello world';
+
+    subject.subscribe(observer);
+    subject.unsubscribe(observer);
+    subject.notify(data);
+
+    expect(observer.update).not.toHaveBeenCalled();
+  });
+
+  test('#Payment should  notify subject after a credit card transaction', () => {
+    const subject = new PaymentSubject();
+    const payment = new Payment(subject);
+
+    const paymentSubjectNotifierSpy = jest.spyOn(
+      subject,
+      subject.notify.name
+    )
+
+    const data = { userName: 'Abhishek', id: 3};
+    payment.creditCard(data);
+
+    expect(paymentSubjectNotifierSpy).toBeCalledWith(data);
+  });
+
+
+  test('#All should notify subscribers after a credit card payment', () => {
+    const subject = new PaymentSubject();
+    const shipment = new Shipment();
+    const marketing = new Marketing();
+
+    const shipmentUpdateFnSpy = jest.spyOn(shipment, shipment.update.name);
+    const marketingUpdateFnSpy = jest.spyOn(marketing, marketing.update.name);
+
+    subject.subscribe(shipment);
+    subject.subscribe(marketing);
+
+    const payment = new Payment(subject);
+    const data = { id: 4, userName: 'mary'};
+    payment.creditCard(data);
+
+    expect(shipmentUpdateFnSpy).toBeCalledWith(data);
+    expect(marketingUpdateFnSpy).toBeCalledWith(data);
+  });
 });
